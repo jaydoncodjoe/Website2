2dfb7a9ea3336c970d4bbc3d036caf170cc0d086
Jaydon <codjoejaydon@yahoo.com>
2020-08-05 10:46:21 -0500

    Hidden

---
+++ index.js
@@ -0,0 +1,24 @@
+function food() {
+  var x = document.getElementById("food");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function drink() {
+  var y = document.getElementById("drink");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
+function side() {
+  var z = document.getElementById("side");
+  if (z.style.display === "none") {
+    z.style.display = "block";
+  } else {
+    z.style.display = "none";
+  }
+}
